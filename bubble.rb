5c497a7376f3e72b8ee347621d254550613fb752
bright okike <brightokike@gmail.com>
2019-06-05 20:59:59 +0300

    definig the method

---
+++ bubble.rb
@@ -0,0 +1,12 @@
+
+def bubble_sorting(arr)
+    n = arr.length
+    for i in (0..(n-1))
+        
+    end
+        
+
+end
+#(1..n).each do |x| arr[x] > arr[x+1]
+arr = [3,1,4,2,0, 5, 7]
+bubble_sorting(arr)
