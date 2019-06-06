33b9f052231fced20026f800a03369ee22818368
bright okike <brightokike@gmail.com>
2019-06-06 14:27:47 +0300

    the final algorithm

--- bubble.rb
+++ bubble.rb
@@ -1,46 +1,32 @@
 def bubble_sorting(arr)
 
     n = arr.length - 1
-    stop = true
-    swapused = false
-
- while stop == true
-        
     n.times do |i|
-
+        swap = true
         i = 0
-
-        first_item = arr[i]
-        second_item = arr[i+1]
-
-        greater = first_item >= second_item
-
-        if greater
-            arr[i] = second_item
-            arr[i+1] = first_item
-            print "greater"
-        
-            swapused = true
+        n = (arr.length-1) - i
+        first_index = 0
+        second_index = 1
+        while second_index <= n
+            if arr[first_index ] > arr[second_index]
+                # temp = arr[first_index ]
+                # arr[first_index ] = arr[second_index]
+                # arr[second_index] = temp
+                arr[first_index], arr[second_index] = arr[second_index], arr[first_index]
+                swap = false
+            end
+            first_index  += 1
+            second_index += 1
         end
-
-        i = i + 1
-        print i
+        if swap == true
+            return arr
+        end
     end
-
-    if swapused == false
-        stop = false
-    else
-        stop = true
-    end
-
- end
-
-
     return arr
 
 end
 
 #(1..n).each do |x| arr[x] > arr[x+1]
-arr = [2,3,4,1]
+arr = [2,3,4,1,26,9,10,4,4, 10, 15,12,2,0,1,5,70,23,57,29,0,12,33,100,33,77]
 print bubble_sorting(arr)
 
